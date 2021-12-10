0afececab1e419d08551caaea6c9be5b038187de
Marco Cusenza <marco.cusenza.me@gmail.com>
2021-12-10 01:03:57 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+  el: "#root",
+  data: {
+    emails: [],
+  },
+  created() {
+    for (let x = 0; x < 10; x++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((resp) => {
+          // this.emails[x] = resp.data.response; //PERCHE' NON FUNZIONA?
+          this.emails.push(resp.data.response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+});
+
